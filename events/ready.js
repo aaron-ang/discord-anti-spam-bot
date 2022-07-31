e6de21e95da17adcab349b92c579fe3aab57e186
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-07-31 14:04:23 +0800

    remove js files

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// When the client is ready, run this code (only once)
-module.exports = {
-    name: "ready",
-    once: true,
-    execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
-    },
-};
